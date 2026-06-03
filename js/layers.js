23b1f930af76e82c7ed002ce14bb879fea726c96
Samakalt152416 <namo.laosomboon1@gmail.com>
2026-06-03 08:33:52 +0700

    z
    z

--- js/layers.js
+++ js/layers.js
@@ -4,10 +4,10 @@ addLayer("c", {
     position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     startData() { return {
         unlocked: true,
-		points: new Decimal(1),
+		points: new ExpantaNum(1),
     }},
     color: "#0c398d",
-    requires: new Decimal(0.001), // Can be a function that takes requirement increases into account
+    requires: new ExpantaNum(0.001), // Can be a function that takes requirement increases into account
     resource: "Consistency", // Name of prestige currency
     baseResource: "points", // Name of resource prestige is based on
     baseAmount() {return player.points}, // Get the current amount of baseResource
